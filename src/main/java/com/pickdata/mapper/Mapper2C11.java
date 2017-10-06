a109f4067f8cfd0a3ae7d8f48fdc1d2847bd3db5
mindo <jeehyun.rek@gmail.com>
2017-10-06 13:12:04 +0900

    test

--- src/main/java/com/pickdata/mapper/Mapper2C11.java
+++ src/main/java/com/pickdata/mapper/Mapper2C11.java
@@ -1,6 +1,5 @@
 package com.pickdata.mapper;
 
-
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
@@ -8,18 +7,28 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import com.pickdata.TaggedKey.TaggedKey;
+import com.pickdata.beta.Score;
+import com.pickdata.parser.BigContestParser;
 
+public class Mapper2C11 extends Mapper<Text, Text, Text, Text> {
 
-public class Mapper2C11 extends Mapper<Text, Text, TaggedKey,Text> {
-	
-	TaggedKey outputKey = new TaggedKey();
+	Text outputKey = new Text();
 	Text outValue = new Text();
-	
+	Score scoreClass = new Score();
+	String columnName = "c11";
+
 	@Override
-	protected void map(Text key, Text value, Context context)
-			throws IOException, InterruptedException {
+	protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 		
+		BigContestParser parser = new BigContestParser(value);
+
+		String customerValue = (String) parser.map.get(columnName);
+
+		outputKey.set(parser.map.get("id") + "");
+		outValue.set(scoreClass.scoreCal(columnName, customerValue) + "");
+
+		context.write(outputKey, outValue);
 		context.write(outputKey, outValue);
 	}
-	
+
 }
