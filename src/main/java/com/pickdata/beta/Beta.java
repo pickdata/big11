d746eeae4691f08ef01a38f6db6559d3a68abf66
mindo <jeehyun.rek@gmail.com>
2017-10-06 13:22:01 +0900

    mapper c11

--- src/main/java/com/pickdata/beta/Beta.java
+++ src/main/java/com/pickdata/beta/Beta.java
@@ -6,90 +6,93 @@ import java.util.Map;
 import java.util.Set;
 
 public class Beta {
-	
+
 	String key = null;
 	Double beta = 0.0;
 	Double minBeta = 0.0;
-	
-	public Map<String, Double> mapt13() {
-		
-		Map<String, Double> mapt13 = new HashMap<String, Double>();
-		
-		mapt13.put("bx", 0.0);
-		mapt13.put("bo", -0.9493);
-		mapt13.put("bk", -1.3525);
-		mapt13.put("bg", 0.8241);
-		mapt13.put("br", 0.0);
-	
-		return mapt13;
-	}
-	
-	public Map<String, Double> mapc11() {
-		
-		Map<String, Double> mapc11 = new HashMap<String, Double>();
 
-		Double[] betas = new Double[] { 1.8613, 0.6827, 0.1108, -0.4673, -0.752, -1.0232, -1.2449, -2.5241, -1.2892,
-				-1.3312, -1.5922, 0.0 };
-
+	public Map<String, Double> mapPutter(Double[] betas) {
+		Map<String, Double> map = new HashMap<String, Double>();
 		for (int i = 0; i < betas.length; i++) {
-			String name = "b" + i;
+			String name = i+"";
 			Double value = betas[i];
-			mapc11.put(name, value);
+			map.put(name, value);
 		}
-		
-		return mapc11;
+		return map;
 	}
-	
-	public Map<String, Double> mapc3() {
-		
-		Map<String, Double> mapc3 = new HashMap<String, Double>();
-
-		Double[] betas = new Double[] { -4.2021,	-3.0299, -2.2288, -1.7538, -1.1574, -0.8809,	-0.7598, 0.0 };
-
-		for (int i = 0; i < betas.length; i++) {
-			String name = "b" + i;
+	public Map<String, Double> mapPutter(Double[] betas, int initialValue, int columnPlusValue) {
+		Map<String, Double> map = new HashMap<String, Double>();
+		for (int i = initialValue; i < betas.length; i++) {
+			String name = i+columnPlusValue+"";
 			Double value = betas[i];
-			mapc3.put(name, value);
+			map.put(name, value);
 		}
-		
-		return mapc3;
+		return map;
 	}
-	
-	public Map<String, Double> mapc9() {
-		
-		Map<String, Double> mapc9 = new HashMap<String, Double>();
-
-		Double[] betas = new Double[] { -0.1807,	-0.2529, 0.4859, 	0.644,	0.8846, 1.1339, 1.2745,	0.7857, -0.1499, 0.4584, 0.1351, 0.0};
-
-		for (int i = 0; i < betas.length; i++) {
-			String name = "b" + i;
+	public Map<String, Double> mapPutter(Double[] betas, int initialValue, String logisticName) {
+		Map<String, Double> map = new HashMap<String, Double>();
+		for (int i = initialValue; i < betas.length; i++) {
+			String name = i+logisticName+"";
 			Double value = betas[i];
-			mapc9.put(name, value);
+			map.put(name, value);
 		}
-		
-		return mapc9;
+		return map;
 	}
-	
-	public Map<String, Double> mapi19() {
+	public Map<String, Double> map(String columnName) {
+		Map<String, Double> returnMap = new HashMap<String, Double>();
+		Map<String, Double> mapT13 = new HashMap<String, Double>();
+//		Double[] betasT13 = new Double[] { 1.8613, 0.6827, 0.1108, -0.4673, -0.752, -1.0232, -1.2449, -2.5241, -1.2892,
+//				-1.3312, -1.5922, 0.0 };
+		mapT13.put("X", 0.0);
+		mapT13.put("O", -0.9493);
+		mapT13.put("K", -1.3525);
+		mapT13.put("G", 0.8241);
+		mapT13.put("R", 0.0);
+
+		// C11 범주값 0 ~ 11 증가값(+1)
+		Map<String, Double> mapC11 = new HashMap<String, Double>();
+		Double[] betasC11 = new Double[] { 1.8613, 0.6827, 0.1108, -0.4673, -0.752, -1.0232, -1.2449, -2.5241, -1.2892,	-1.3312, -1.5922, 0.0 };
+		mapC11 = mapPutter(betasC11);
 		
-		Map<String, Double> mapi19 = new HashMap<String, Double>();
+		// C3 범주값 0 ~ 7 증가값(+1)
+		Map<String, Double> mapC3 = new HashMap<String, Double>();
+		Double[] betasC3 = new Double[] { -4.2021, -3.0299, -2.2288, -1.7538, -1.1574, -0.8809, -0.7598, 0.0 };
+		mapC3 = mapPutter(betasC3);
 
-		Double[] betas = new Double[] { -2.3813, -2.1266, -2.0, -1.6275, -1.4356, -1.2816, -1.2377, -0.5328, 0.0};
-
-		for (int i = 0; i < betas.length; i++) {
-			String name = "b" + i;
-			Double value = betas[i];
-			mapi19.put(name, value);
+		// C9  범주값 0, 1 ~ 121 증가값(+12) 
+		Map<String, Double> mapC9 = new HashMap<String, Double>();
+		Double[] betasC9 = new Double[] { -0.1807, -0.2529, 0.4859, 0.644, 0.8846, 1.1339, 1.2745, 0.7857, -0.1499,	0.4584, 0.1351, 0.0 };
+		mapC9.put("0", betasC9[0]);
+		// method overloading
+		mapC9 = mapPutter(betasC9,1,12);
+		
+		// I19 범주값 0,10미만,20미만,30미만,40미만,50미만,60미만,70미만,80미만,90미만,90이상
+		Map<String, Double> mapI19 = new HashMap<String, Double>();
+		Double[] betasI19 = new Double[] { -2.3813, -2.1266, -2.0, -1.6275, -1.4356, -1.2816, -1.2377, -0.5328, 0.0 };
+		mapI19.put("0", betasI19[0]);
+		// method overloading
+		mapI19 = mapPutter(betasI19,1,"0미만");
+		mapI19.put("90이상", betasI19[betasI19.length]);
+		
+		if(columnName.equals("t13")||columnName.equals("T13")){
+			returnMap = mapT13;
+		} else if(columnName.equals("c11")||columnName.equals("C11")){
+			returnMap = mapC11;
+		} else if(columnName.equals("c3")||columnName.equals("C3")){
+			returnMap = mapC3;
+		} else if(columnName.equals("c9")||columnName.equals("C9")){
+			returnMap = mapC9;
+		} else if(columnName.equals("i19")||columnName.equals("I19")){
+			returnMap = mapI19;
 		}
-		
-		return mapi19;
-	}	
+		return returnMap; 
+	}
 
 	public Double minBeta(Map<String, Double> map) {
-		
+
 		Set<Map.Entry<String, Double>> entrySet = map.entrySet();
 		Iterator<Map.Entry<String, Double>> entryIterator = entrySet.iterator();
-		
+
 		while (entryIterator.hasNext()) {
 			Map.Entry<String, Double> entry = entryIterator.next();
 			beta = entry.getValue();
@@ -97,7 +100,7 @@ public class Beta {
 				minBeta = beta;
 			}
 		}
-		
+
 		return minBeta;
 	}
 }
--- src/main/java/com/pickdata/mapper/Mapper2C11.java
+++ src/main/java/com/pickdata/mapper/Mapper2C11.java
@@ -2,11 +2,9 @@ package com.pickdata.mapper;
 
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import com.pickdata.TaggedKey.TaggedKey;
 import com.pickdata.beta.Score;
 import com.pickdata.parser.BigContestParser;
 
@@ -21,13 +19,11 @@ public class Mapper2C11 extends Mapper<Text, Text, Text, Text> {
 	protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 		
 		BigContestParser parser = new BigContestParser(value);
-
 		String customerValue = (String) parser.map.get(columnName);
 
 		outputKey.set(parser.map.get("id") + "");
 		outValue.set(scoreClass.scoreCal(columnName, customerValue) + "");
 
-		context.write(outputKey, outValue);
 		context.write(outputKey, outValue);
 	}
 
