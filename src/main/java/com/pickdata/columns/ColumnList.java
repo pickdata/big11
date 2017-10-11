ed366b52e67ccf7f6b7cfa3c9825d37eee5e8444
java@guestBig <kkolx2@nate.com>
2017-10-11 21:07:38 +0900

    * error c9, c10, c12, age, t13 	 * success: c1, c2, c3, c4, c11, c13, sex, i9, i16, i19, t11

--- src/main/java/com/pickdata/columns/ColumnList.java
+++ src/main/java/com/pickdata/columns/ColumnList.java
@@ -2,7 +2,11 @@ package com.pickdata.columns;
 
 public class ColumnList {
 
+	//error c9, c10, c12, age, t13
 //	public static String[] columnName = {"c1", "c2"};
-	public static String[] columnName = {"c5", "c5", };
+	/*
+	 * c1, c2, c3, c4, c11, c13, sex, i9, i16, i19, t11
+	 */
+	public static String[] columnName = {"t14"};
 	public static int idcnt;
 }
